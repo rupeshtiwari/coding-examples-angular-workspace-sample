9321e50e9405cdc20ce2845b7adb258c5a018c9d
rupeshtiwari <roopkt@gmail.com>
2021-05-05 17:29:31 -0400

    fix: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { UiLoginComponent } from 'projects/ui-login/src/public-api';
 import { CoursesComponent } from './coures/courses/courses.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'courses', pathMatch: 'full' },
-  { path: 'fsms-courses-angular-app', redirectTo: 'courses', pathMatch: 'full' },
+  {
+    path: 'fsms-courses-angular-app',
+    redirectTo: 'courses',
+    pathMatch: 'full',
+  },
   { path: 'courses', component: CoursesComponent },
   { path: 'login', component: UiLoginComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { LayoutModule } from '@angular/cdk/layout';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from 'projects/core-data/src/lib/in-memory-data.service';
 import { CoreDataModule } from 'projects/core-data/src/public-api';
 import { MaterialModule } from 'projects/material/src/public-api';
 import { UiLoginModule } from 'projects/ui-login/src/public-api';
 import { UiToolbarModule } from 'projects/ui-toolbar/src/public-api';
-import { InMemoryDataService } from 'projects/core-data/src/lib/in-memory-data.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesModule } from './coures/courses/courses.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,9 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     MaterialModule,
     CoursesModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
-      apiBase: 'api/',
-    }),
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {}),
   ],
   providers: [],
   bootstrap: [AppComponent],
